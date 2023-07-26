1ad3d164028f26f76b005d23a0b0a0776544e902
Arthur Lima <arthurlima46@gmail.com>
2023-07-26 18:27:51 -0300

    Prevenindo erros no PUT

--- Api/Api/Controllers/LembretesController.cs
+++ Api/Api/Controllers/LembretesController.cs
@@ -9,6 +9,7 @@ using Api.Models;
 using Api.DbContexts;
 using Api.Base;
 using Microsoft.IdentityModel.Tokens;
+using System.Text.RegularExpressions;
 
 namespace Api.Controllers
 {
@@ -59,14 +60,20 @@ namespace Api.Controllers
         [HttpPut("{id}")]
         public IActionResult PutLembrete(string? id, Lembrete lembrete)
         {
-            if (id != lembrete.Id)
+            if(id != lembrete.Id)
+                return NotFound("Id not founded");
+
+            Regex validateDateRegex = new Regex("^(((0[1-9]|[12]\\d|3[01])\\/(0[13578]|1[02])\\/((19|[2-9]\\d)\\d{2}))|((0[1-9]|[12]\\d|30)\\/(0[13456789]|1[012])\\/((19|[2-9]\\d)\\d{2}))|((0[1-9]|1\\d|2[0-8])\\/02\\/((19|[2-9]\\d)\\d{2}))|(29\\/02\\/((1[6-9]|[2-9]\\d)(0[48]|[2468][048]|[13579][26])|((16|[2468][048]|[3579][26])00))))$");
+            bool success = validateDateRegex.IsMatch(lembrete.Date);
+
+            if (success == false)
             {
-                return BadRequest();
+                return BadRequest(error: "The date has to be a valid date like the example: \"dd/MM/yyyy\"");
             }
 
             try
             {
-                this.Insert(lembrete);
+                this.Update(lembrete);
             }
             catch (DbUpdateConcurrencyException)
             {
